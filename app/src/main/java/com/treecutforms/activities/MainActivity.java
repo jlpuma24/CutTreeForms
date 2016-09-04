c4bea0786f7268790bb68d99e826d65d31af4c1c
Gabriel Rodriguez <gabriel.rodriguez3103@gmail.com>
2016-09-03 21:22:59 -0500

    :star: Add floating action button to launch Form Activity

--- app/src/main/java/com/treecutforms/activities/MainActivity.java
+++ app/src/main/java/com/treecutforms/activities/MainActivity.java
@@ -1,15 +1,21 @@
 package com.treecutforms.activities;
 
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
+import android.view.View;
 
 import com.treecutforms.R;
 import com.treecutforms.adapters.FormItemAdapter;
 import com.treecutforms.databinding.ActivityMainBinding;
+import com.treecutforms.models.Form;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ActivityMainBinding binding;
     private FormItemAdapter adapter = new FormItemAdapter();
@@ -18,8 +24,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        setupToolbar();
+        setupFloatingButton();
         setupAdapter();
         setupRecyclerView();
+    }
+
+    /**
+     * Setup activity main toolbar
+     */
+    private void setupToolbar() {
+        binding.toolbar.setTitle(getString(R.string.activity_main_toolbar_title));
+        binding.toolbar.setTitleTextColor(Color.WHITE);
+    }
+
+    private void setupFloatingButton() {
+        binding.fabNewForm.setOnClickListener(this);
     }
 
     /**
@@ -27,14 +47,20 @@ public class MainActivity extends AppCompatActivity {
      */
     private void setupAdapter() {
         // TODO get real data from local database
-//        ArrayList<Form> forms = new ArrayList<>();
-//        forms.add(new Form());
-//        forms.add(new Form());
-//        forms.add(new Form());
-//        forms.add(new Form());
-//        forms.add(new Form());
-//        forms.add(new Form());
-//        adapter.setItems(forms);
+        ArrayList<Form> forms = new ArrayList<>();
+        forms.add(new Form());
+        forms.add(new Form());
+        forms.add(new Form());
+        forms.add(new Form());
+        forms.add(new Form());
+        forms.add(new Form());
+        forms.add(new Form());
+        forms.add(new Form());
+        forms.add(new Form());
+        forms.add(new Form());
+        forms.add(new Form());
+        forms.add(new Form());
+        adapter.setItems(forms);
     }
 
     /**
@@ -46,4 +72,16 @@ public class MainActivity extends AppCompatActivity {
         binding.rvFormList.setAdapter(adapter);
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.fab_new_form:
+                launchFormActivity();
+                break;
+        }
+    }
+
+    private void launchFormActivity() {
+        startActivity(new Intent(this, FormActivity.class));
+    }
 }
